09e897b968aab89c51fdd77496e4815e485e73be
machacjakub <j.machac@post.cz>
2022-07-17 23:48:57 +0200

    In dev. + email

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <>
-    <h1 style={{textAlign: "center"}}>IN DEVELOPMENT</h1>
+      <h1 style={{textAlign: "center"}}>IN DEVELOPMENT</h1>
+      <p style={{margin: "40px"}}>mail@jakubmachac.cz</p>
     </>
   );
 }
